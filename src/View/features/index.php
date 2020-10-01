aff54bdf30f16670e414738faa858af05f448de3
unknown <radek.malysa@gmail.com>
2020-10-01 21:56:34 +0200

    html template

--- src/View/features/index.php
+++ src/View/features/index.php
@@ -1,18 +1,45 @@
 <section class="section">
-    <div class="section-inner pv7 ph3 center">
+    <div class="section-inner pv7 ph3 ph5-m center">
         <header>
-            <h2 class="fw4 f2 ma0 tc">Why Choose Us</h2>
+            <h2 class="fw4 f3 f2-ns ma0 tc">Why Choose Us Why Choose Us</h2>
+            <p class="f5 f4-ns mt3 mt4-ns mb1 tc">Still have some hesitations whether cooperation with us is worth the trouble? Check the reasons why you should choose us among other companies!</p>
         </header>
-        <div class="nl3 nr3 mt5 flex flex-wrap justify-center">
-            <div class="w-third ph3">
+        <div class="nl3 nr3 flex flex-wrap justify-center-l">
+            <div class="w-100 w-50-m w-third-l ph3 mt4 mt5-ns">
                 <div class="flex flex-column">
-                    <div class="tc mb4">
-                        <img class="dib ico-feature" src="/img/img1.svg" alt="" />
+                    <div class="tc mb3 mb4-ns">
+                        <img class="dib ico-feature" src="/img/ico1.svg" alt="" />
                     </div>
-                    <div class="tc f3 mb4">
+                    <div class="tc f4 f3-ns mb3 mb4-ns">
                         Experience
                     </div>
-                    <div class="tc f5 lh-copy">
+                    <div class="tc f6">
+                        We have extensive experience and can be proud of  100+ completed projects.
+                    </div>
+                </div>
+            </div>
+            <div class="w-100 w-50-m w-third-l ph3 mt4 mt5-ns">
+                <div class="flex flex-column">
+                    <div class="tc mb3 mb4-ns">
+                        <img class="dib ico-feature" src="/img/ico2.svg" alt="" />
+                    </div>
+                    <div class="tc f4 f3-ns mb3 mb4-ns">
+                        Experience
+                    </div>
+                    <div class="tc f6">
+                        We have extensive experience and can be proud of  100+ completed projects.
+                    </div>
+                </div>
+            </div>
+            <div class="w-100 w-50-m w-third-l ph3 mt4 mt5-ns">
+                <div class="flex flex-column">
+                    <div class="tc mb3 mb4-ns">
+                        <img class="dib ico-feature" src="/img/ico1.svg" alt="" />
+                    </div>
+                    <div class="tc f4 f3-ns mb3 mb4-ns">
+                        Experience
+                    </div>
+                    <div class="tc f6">
                         We have extensive experience and can be proud of  100+ completed projects.
                     </div>
                 </div>
